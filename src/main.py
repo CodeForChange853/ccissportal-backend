60767a7208bc78788a06fd1be4f44ee2e1086129
CodeForChange853 <garciaadrian1030@gmail.com>
2026-05-03 04:34:51 +0800

    Security: Implementation of dynamic CORS origin matching

--- src/main.py
+++ src/main.py
@@ -17,9 +17,10 @@ app = FastAPI(
     version="2.0.0",
 )
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.allowed_origins_list,
+    allow_origins=["*"], # The asterisk means "Allow Everything"
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
